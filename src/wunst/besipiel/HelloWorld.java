8780dbb399146efa33981ed9da4e7ab3d05d57fa
Wunst <Wunst@DESKTOP-44LFM0L.fritz.box>
2019-06-22 11:37:46 +0200

    Added parameter output to HelloWorld.java

--- src/wunst/besipiel/HelloWorld.java
+++ src/wunst/besipiel/HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		if(args.length <= 0) return;
+		System.out.printf("Hello! The first argument you gave to this method was %s!", args[0]);
 	}
 }
